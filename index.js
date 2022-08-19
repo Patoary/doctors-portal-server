6cbf8ed88ff9b8ac2f55bd378f23e7da2514cf66
patoary <zahidsagar93@gmail.com>
2022-08-19 18:02:58 +0900

    write some conditions in put user/admin/:email api because of that only admin can make a new admin

--- index.js
+++ index.js
@@ -47,14 +47,21 @@ async function run() {
             res.send(users);
         });
 
-        app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccout = await userCollection.findOne({ email: requester });
+            if (requesterAccout.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }else{
+                res.status(403).send({message: 'forbidden'});
+            }
+
         });
 
         app.put('/user/:email', async (req, res) => {
